edb21f992b198c810110c2a96cad9ababc4a02fd
shisotem <129283841+shisotem@users.noreply.github.com>
2023-10-14 12:16:50 +0900

    test: テスト項目を追加

--- tutorials/jest/jest-tutorial/isZero.test.ts
+++ tutorials/jest/jest-tutorial/isZero.test.ts
@@ -4,3 +4,8 @@ test("0を渡したらtrueになること", () => {
   const result = isZero(0);
   expect(result).toBe(true);
 });
+
+test("1を渡したらfalseになること", () => {
+  const result = isZero(1);
+  expect(result).toBe(false);
+});
