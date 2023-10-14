817392b7cc8643eb03484a3db2026c414c340dcb
shisotem <129283841+shisotem@users.noreply.github.com>
2023-10-14 14:43:23 +0900

    test: OFFと表示されていることを確かめる

--- tutorials/component-test/component-test-tutorial/src/SimpleButton.test.tsx
+++ tutorials/component-test/component-test-tutorial/src/SimpleButton.test.tsx
@@ -1,6 +1,8 @@
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import { SimpleButton } from "./SimpleButton";
 
 test("ボタンをクリックするとON/OFFの表示が切り替わる", async () => {
   render(<SimpleButton />);
+  const simpleButton = screen.getByRole("button");
+  expect(simpleButton).toHaveTextContent("OFF");
 });
