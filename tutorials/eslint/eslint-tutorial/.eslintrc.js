0eea5b4d92818e7c19cf103fc2ea6ec3847f2af4
shisotem <129283841+shisotem@users.noreply.github.com>
2023-10-14 18:30:16 +0900

    chore: eslintのコンフィグファイルを作成

---
+++ tutorials/eslint/eslint-tutorial/.eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  rules: {
+    "no-console": "error",
+    camelcase: ["error", { properties: "never" }],
+  },
+};
