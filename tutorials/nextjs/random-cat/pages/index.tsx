ed7e260febc52190d045fffeb17468ea594a4a63
shisotem <129283841+shisotem@users.noreply.github.com>
2023-10-14 09:30:39 +0900

    refactor: CSR -> SSR

--- tutorials/nextjs/random-cat/pages/index.tsx
+++ tutorials/nextjs/random-cat/pages/index.tsx
@@ -1,22 +1,26 @@
-import { NextPage } from "next";
-import { useEffect, useState } from "react";
+import { GetServerSideProps, NextPage } from "next";
+import { useState } from "react";
 
-const IndexPage: NextPage = () => {
-  // 状態を定義
-  const [imageUrl, setImageUrl] = useState("");
-  const [loading, setLoading] = useState(true);
-  useEffect(() => {
-    fetchImage().then((newImage) => {
-      setImageUrl(newImage.url);
-      setLoading(false);
-    });
-  }, []);
-  // ボタンクリック時に画像を読み込む処理
+// getServerSidePropsから渡されるpropsの型
+type Props = {
+  initialImageUrl: string;
+};
+
+// propsを受け取る引数を追加
+const IndexPage: NextPage<Props> = ({ initialImageUrl }) => {
+  const [imageUrl, setImageUrl] = useState(initialImageUrl); // 初期値を渡す
+  const [loading, setLoading] = useState(false); // 初期状態はfalseにしておく
+  // useEffect(() => {
+  //   fetchImage().then((newImage) => {
+  //     setImageUrl(newImage.url);
+  //     setLoading(false);
+  //   });
+  // }, []);
   const handleClick = async () => {
-    setLoading(true); // 読み込み中フラグを立てる
+    setLoading(true);
     const newImage = await fetchImage();
-    setImageUrl(newImage.url); // 画像URLの状態を更新
-    setLoading(false); // 読み込み中フラグを倒す
+    setImageUrl(newImage.url);
+    setLoading(false);
   };
   return (
     <div>
@@ -26,6 +30,16 @@ const IndexPage: NextPage = () => {
   );
 };
 export default IndexPage;
+
+// サーバーサイドで実行する処理
+export const getServerSideProps: GetServerSideProps<Props> = async () => {
+  const image = await fetchImage();
+  return {
+    props: {
+      initialImageUrl: image.url,
+    },
+  };
+};
 
 type Image = {
   url: string;
